e95077a8255f7042f9414d4570b03ab984fd9587
Troy Topnik <troyt@activestate.com>
2015-05-29 14:01:54 -0700

    git subrepo pull apps/wordpress
    subrepo:
      subdir:   "apps/wordpress"
      merged:   "b0edf0b"
    upstream:
      origin:   "git@github.com:Stackato-Apps/wordpress"
      branch:   "stackato-v3"
      commit:   "b0edf0b"
    git-subrepo:
      version:  "0.2.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "b136967"

--- apps/wordpress/wp-includes/js/customize-preview.js
+++ apps/wordpress/wp-includes/js/customize-preview.js
@@ -1,10 +1,12 @@
-<<<<<<< HEAD
-(function(b,d){var c=wp.customize,a;a=function(g,e,f){var h;return function(){var i=arguments;f=f||this;clearTimeout(h);h=setTimeout(function(){h=null;g.apply(f,i)},e)}};c.Preview=c.Messenger.extend({initialize:function(g,f){var e=this;c.Messenger.prototype.initialize.call(this,g,f);this.body=d(document.body);this.body.on("click.preview","a",function(h){h.preventDefault();e.send("scroll",0);e.send("url",d(this).prop("href"))});this.body.on("submit.preview","form",function(h){h.preventDefault()});this.window=d(window);this.window.on("scroll.preview",a(function(){e.send("scroll",e.window.scrollTop())},200));this.bind("scroll",function(h){e.window.scrollTop(h)})}});d(function(){c.settings=window._wpCustomizeSettings;if(!c.settings){return}var f,e;f=new c.Preview({url:window.location.href,channel:c.settings.channel});f.bind("settings",function(g){d.each(g,function(i,h){if(c.has(i)){c(i).set(h)}else{c.create(i,h)}})});f.trigger("settings",c.settings.values);f.bind("setting",function(g){var h;g=g.slice();if(h=c(g.shift())){h.set.apply(h,g)}});f.bind("sync",function(g){d.each(g,function(i,h){f.trigger(i,h)});f.send("synced")});f.bind("active",function(){if(c.settings.nonce){f.send("nonce",c.settings.nonce)}});f.send("ready");e=d.map(["color","image","position_x","repeat","attachment"],function(g){return"background_"+g});c.when.apply(c,e).done(function(j,i,m,h,l){var n=d(document.body),o=d("head"),g=d("#custom-background-css"),k;if(n.hasClass("custom-background")&&!g.length){return}k=function(){var p="";n.toggleClass("custom-background",!!(j()||i()));if(j()){p+="background-color: "+j()+";"}if(i()){p+='background-image: url("'+i()+'");';p+="background-position: top "+m()+";";p+="background-repeat: "+h()+";";p+="background-position: top "+l()+";"}g.remove();g=d('<style type="text/css" id="custom-background-css">body.custom-background { '+p+" }</style>").appendTo(o)};d.each(arguments,function(){this.bind(k)})})})})(wp,jQuery);
-=======
 (function( exports, $ ){
 	var api = wp.customize,
 		debounce;
 
+	/**
+	 * Returns a debounced version of the function.
+	 *
+	 * @todo Require Underscore.js for this file and retire this.
+	 */
 	debounce = function( fn, delay, context ) {
 		var timeout;
 		return function() {
@@ -20,6 +22,12 @@
 		};
 	};
 
+	/**
+	 * @constructor
+	 * @augments wp.customize.Messenger
+	 * @augments wp.customize.Class
+	 * @mixes wp.customize.Events
+	 */
 	api.Preview = api.Messenger.extend({
 		/**
 		 * Requires params:
@@ -59,14 +67,14 @@
 		if ( ! api.settings )
 			return;
 
-		var preview, bg;
+		var bg;
 
-		preview = new api.Preview({
+		api.preview = new api.Preview({
 			url: window.location.href,
 			channel: api.settings.channel
 		});
 
-		preview.bind( 'settings', function( values ) {
+		api.preview.bind( 'settings', function( values ) {
 			$.each( values, function( id, value ) {
 				if ( api.has( id ) )
 					api( id ).set( value );
@@ -75,9 +83,9 @@
 			});
 		});
 
-		preview.trigger( 'settings', api.settings.values );
+		api.preview.trigger( 'settings', api.settings.values );
 
-		preview.bind( 'setting', function( args ) {
+		api.preview.bind( 'setting', function( args ) {
 			var value;
 
 			args = args.slice();
@@ -86,19 +94,36 @@
 				value.set.apply( value, args );
 		});
 
-		preview.bind( 'sync', function( events ) {
+		api.preview.bind( 'sync', function( events ) {
 			$.each( events, function( event, args ) {
-				preview.trigger( event, args );
+				api.preview.trigger( event, args );
 			});
-			preview.send( 'synced' );
+			api.preview.send( 'synced' );
 		});
 
-        preview.bind( 'active', function() {
-            if ( api.settings.nonce )
-                preview.send( 'nonce', api.settings.nonce );
-        });
+		api.preview.bind( 'active', function() {
+			if ( api.settings.nonce ) {
+				api.preview.send( 'nonce', api.settings.nonce );
+			}
 
-		preview.send( 'ready' );
+			api.preview.send( 'documentTitle', document.title );
+		});
+
+		api.preview.send( 'ready', {
+			activePanels: api.settings.activePanels,
+			activeSections: api.settings.activeSections,
+			activeControls: api.settings.activeControls
+		} );
+
+		// Display a loading indicator when preview is reloading, and remove on failure.
+		api.preview.bind( 'loading-initiated', function () {
+			$( 'body' ).addClass( 'wp-customizer-unloading' );
+			$( 'html' ).prop( 'title', api.settings.l10n.loading );
+		});
+		api.preview.bind( 'loading-failed', function () {
+			$( 'body' ).removeClass( 'wp-customizer-unloading' );
+			$( 'html' ).prop( 'title', '' );
+		});
 
 		/* Custom Backgrounds */
 		bg = $.map(['color', 'image', 'position_x', 'repeat', 'attachment'], function( prop ) {
@@ -139,7 +164,8 @@
 				this.bind( update );
 			});
 		});
+
+		api.trigger( 'preview-ready' );
 	});
 
 })( wp, jQuery );
->>>>>>> 3.5.1
